bd159e6aeb83c054d4e503e41ecc869c47cbd696
allen_kuo <v155640@gmail.com>
2019-06-11 16:09:22 +0800

    better way to implement

--- app/src/main/java/android/interview/fibonacci/Fibonacci.java
+++ app/src/main/java/android/interview/fibonacci/Fibonacci.java
@@ -3,6 +3,7 @@ package android.interview.fibonacci;
 import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Fibonacci {
 
@@ -15,28 +16,26 @@ public class Fibonacci {
     public ArrayList<Integer> getResultList() {
         ArrayList<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < mIndex; i++) {
-            list.add(calculate_iterative(i));
-        }
+        calculate_iterative(mIndex, list);
 
         return list;
     }
 
     /*
-    * Using recursive approach
-    *
-    * Time Complexity : O(2^n)
-    *
-    * For upper bound:
-    *
-    * T(n) = T(n-1) + T(n-2) + c
-    *      = 2T(n-1) + c    //from the approximation T(n-1) ~ T(n-2)
-    *      = 2*(2T(n-2) + c) + c
-    *      = 4T(n-2) + 3c
-    *      = 8T(n-3) + 7c
-    *      = 2^k * T(n - k) + (2^k - 1)*c
-    *
-    * **/
+     * Using recursive approach
+     *
+     * Time Complexity : O(2^n)
+     *
+     * For upper bound:
+     *
+     * T(n) = T(n-1) + T(n-2) + c
+     *      = 2T(n-1) + c    //from the approximation T(n-1) ~ T(n-2)
+     *      = 2*(2T(n-2) + c) + c
+     *      = 4T(n-2) + 3c
+     *      = 8T(n-3) + 7c
+     *      = 2^k * T(n - k) + (2^k - 1)*c
+     *
+     * **/
     private int calculate_recursive(int n) {
         if (n < 2) {
             return n;
@@ -57,12 +56,29 @@ public class Fibonacci {
         int value2 = 1;
         int result = n;
 
-        for(int i=2 ; i<=n ; i++) {
+        for (int i = 2; i <= n; i++) {
             result = value1 + value2;
             value1 = value2;
             value2 = result;
         }
 
         return result;
+    }
+
+    private void calculate_iterative(int n, List<Integer> list) {
+        list.clear();
+        int value1 = 0;
+        int value2 = 1;
+        int result = n;
+        if (n >= 0) list.add(0);
+        if (n >= 1) list.add(1);
+
+        for (int i = 2; i <= n; i++) {
+            result = value1 + value2;
+            list.add(result);
+            value1 = value2;
+            value2 = result;
+        }
+
     }
 }
